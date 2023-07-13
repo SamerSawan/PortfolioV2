8c27cb367103a9ae85ce0b7c04ee9f408d53203d
Samer Sawan <67536733+SamerSawan@users.noreply.github.com>
2023-07-12 22:12:35 -0400

    modified resume title

--- app/resume/page.js
+++ app/resume/page.js
@@ -1,3 +1,10 @@
+export const metadata = {
+    title: "Samer Sawan | Resume",
+    description: "Samer Sawan's Resume",
+    author: "Samer Sawan",
+    keywords: "Samer, Sawan, Samer Sawan",
+  }
+
 const page = () => {
     return (
         <iframe className="w-screen h-screen" src='/resume.pdf' />
